61ad3a5d7494576aff6dc26255f7c3c7ef2f17fb
sangeetha parthiban <dineshkumar427585@gmail.com>
2026-07-27 10:28:41 +0530

    lastest seo

--- src/utils/toastConfig.jsx
+++ src/utils/toastConfig.jsx
@@ -1,7 +1,6 @@
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// Default toast configuration
 export const defaultToastConfig = {
   position: "top-right",
   autoClose: 3000,
@@ -13,46 +12,29 @@ export const defaultToastConfig = {
   theme: "dark",
 };
 
-// Custom toast functions
 export const showToast = {
-  // Success toast
   success: (message, options = {}) => {
     toast.success(message, { ...defaultToastConfig, ...options });
   },
-  
-  // Error toast
   error: (message, options = {}) => {
     toast.error(message, { ...defaultToastConfig, ...options });
   },
-  
-  // Warning toast
   warning: (message, options = {}) => {
     toast.warning(message, { ...defaultToastConfig, ...options });
   },
-  
-  // Info toast
   info: (message, options = {}) => {
     toast.info(message, { ...defaultToastConfig, ...options });
   },
-  
-  // Loading toast
   loading: (message, options = {}) => {
-    return toast.loading(message, {
-      ...defaultToastConfig,
-      autoClose: false,
-      ...options
-    });
+    return toast.loading(message, { ...defaultToastConfig, autoClose: false, ...options });
   },
-  
-  // Dismiss all toasts
   dismiss: () => {
     toast.dismiss();
   },
-  
-  // Dismiss specific toast
   dismissById: (id) => {
     toast.dismiss(id);
   }
 };
 
+// Default export to fix Vercel build
 export default showToast;
