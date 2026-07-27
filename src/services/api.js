e1033c2e1217f6347b17fb6c98a5c692e64a70e9
sangeetha parthiban <dineshkumar427585@gmail.com>
2026-07-27 13:08:04 +0530

    lastest seo

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   'https://artistproject-backend.vercel.app/api';
+  
 
 console.log('🌐 API URL:', API_BASE_URL);
 
