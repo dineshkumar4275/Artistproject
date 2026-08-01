f461c85fe0293df967f59a48e55205efad922898
sangeetha parthiban <dineshkumar427585@gmail.com>
2026-08-01 16:27:07 +0530

    Fix favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ function Navbar({ currentPage, setCurrentPage, isAdminLoggedIn, onLogout }) {
           <a href="/" onClick={handleLogoClick} className="logo-link">
             {/* ✅ REPLACED TEXT WITH IMAGE */}
             <img 
-              src="/logo.png" 
+              src="/components/logo.png" 
               alt="KameshFineArt" 
               className="logo-image"
             />
